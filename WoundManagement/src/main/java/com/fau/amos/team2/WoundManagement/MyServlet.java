2613bfd758ef011f49178a240a41d34a409d60da
ub54ymog <stefan_betz91@freenet.de>
2014-04-27 18:48:37 +0200

    Removed vaadin/mgwt Testapps. Created WoundmanagementApp.

---
+++ WoundManagement/src/main/java/com/fau/amos/team2/WoundManagement/MyServlet.java
@@ -0,0 +1,28 @@
+package com.fau.amos.team2.WoundManagement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.addon.touchkit.server.TouchKitServlet;
+import com.vaadin.server.ServiceException;
+import com.vaadin.server.SessionInitEvent;
+import com.vaadin.server.SessionInitListener;
+
+@SuppressWarnings("serial")
+@WebServlet("/*")
+public class MyServlet extends TouchKitServlet {
+    
+    private MyUIProvider uiProvider = new MyUIProvider();
+    
+    @Override
+    protected void servletInitialized() throws ServletException {
+        super.servletInitialized();
+        getService().addSessionInitListener(new SessionInitListener() {
+            @Override
+            public void sessionInit(SessionInitEvent event) throws ServiceException {
+                event.getSession().addUIProvider(uiProvider);
+            }
+        });
+    }
+
+}
