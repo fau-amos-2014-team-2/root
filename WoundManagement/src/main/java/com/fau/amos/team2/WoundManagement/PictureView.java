a9033d9a8e103c938d75013ae3a84947ad707364
simonj <simon.jordan@gmx.de>
2014-05-07 13:29:09 +0200

    alte Versionen entfernt, neue Version eingefuegt

--- WoundManagement/src/main/java/com/fau/amos/team2/WoundManagement/PictureView.java
+++ WoundManagement/src/main/java/com/fau/amos/team2/WoundManagement/PictureView.java
@@ -1,5 +1,6 @@
 package com.fau.amos.team2.WoundManagement;
 
+import com.fau.amos.team2.WoundManagement.BodyWoundSelector.WoundManager;
 import com.vaadin.addon.touchkit.ui.NavigationView;
 import com.vaadin.ui.CssLayout;
 
@@ -18,6 +19,10 @@ public class PictureView extends NavigationView {
 		setCaption("Picture View");
 		
 		CssLayout content = new CssLayout();
+		
+		WoundManager woundManager = new WoundManager(null);
+		
+		content.addComponent(woundManager.getWoundSelector());
 		
 		setContent(content);
 	}
